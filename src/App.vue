a30930aae9ea08f37cfbd3c1745e1d4972fe028b
Albert <albertaliev777@gmail.com>
2026-05-19 10:01:37 +0500

    Random number generator setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const random = () => Math.floor(Math.random() * 100);
+
+const randomValue = ref(random());
+</script>
+
+<template>
+  <h1>Random value persistence</h1>
+  <h2>{{ randomValue }}</h2>
+  <button @click="randomValue = random()">Generate!</button>
+</template>
+
+<style scoped></style>
